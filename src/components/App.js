505503a109f609ff2c042787e6acc68bab87da52
HruthVik <hruthviknaik03@gmail.com>
2024-04-11 20:15:10 +0530

    Your commit message

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world testing</h1>
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ContactList = () => {
+const ContactCard = () => {
     return (
         <div>
         </div>
     )
 }
 
-export default ContactList
+export default ContactCard
